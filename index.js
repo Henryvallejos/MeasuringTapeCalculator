2374ca1750c14c58548bb885f2122d239760105e
Henry Vallejos <59813430+Henryvallejos@users.noreply.github.com>
2026-03-19 19:46:26 -0400

    Updated display input
    Updated display function to only display results

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
 function displayZero() {
     displayData.textContent += 0
+    currentData += 0
 }
 
 function displayOne() {
     displayData.textContent += 1
+    currentData += 1
 }
 
 function displayTwo() {
     displayData.textContent += 2
+    currentData += 2
 }
 
 function displayThree() {
     displayData.textContent += 3
+    currentData += 3
 }
 
 function displayFour() {
     displayData.textContent += 4
+    currentData += 4
 }
 
 function displayFive() {
     displayData.textContent += 5
+    currentData += 5
 }
 
 function displaySix() {
     displayData.textContent += 6
+    currentData += 6
 }
 
 function displaySeven() {
     displayData.textContent += 7
+    currentData += 7
 }
 
 function displayEight() {
     displayData.textContent += 8
+    currentData += 8
 }
 
 function displayNine() {
     displayData.textContent += 9
+    currentData += 9
 }
 
 function displayAdditionSign() {
     displayData.textContent += "+"
+    currentData += "+"
 }
 
 function displaySubtractionSign() {
     displayData.textContent += "-"
+    currentData += "-"
 }
 
 function add(a, b) {
@@ -61,10 +73,16 @@ function calculate() {
 
     if (user_input.includes("+")) {
         const [a, b] = user_input.split("+").map(Number)
-        displayData.textContent += ":" + add(a, b)
+        displayData.textContent = add(a, b)
+        currentData += ":"
+
+        console.log(currentData)
+    
     } else if (user_input.includes("-")) {
         const [a, b] = user_input.split("-").map(Number)
         displayData.textContent += ":" + subtract(a, b)
+        currentData += ":"
+        console.log(currentData)
     } 
 
 }
@@ -72,6 +90,7 @@ function calculate() {
 
 function clearData() {
     displayData.textContent = ""
+    currentData = ""
 }
 
 function displayAri() {
@@ -82,6 +101,6 @@ let halfInch = 0.5
 let quarterInch = .25
 let threeQuarter = .75
 
-let previus
+let currentData = document.getElementById("current-data-el")
 let displayData = document.getElementById("display-data-el")
 displayData.textContent = ""
